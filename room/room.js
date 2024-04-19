cf4c86d468aff99d76266f2f6a7541c0e5a28f2e
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-19 14:01:46 +0530

    room features

---
+++ room/room.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('join room', (room) => {
+        socket.join(room);
+        console.log(`User joined room ${room}`);
+        socket.emit('room joined', room);
+    });
+
+    socket.on('chat message', (msg, room) => {
+        io.to(room).emit('chat message', msg); // Send message to all clients in the room
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
